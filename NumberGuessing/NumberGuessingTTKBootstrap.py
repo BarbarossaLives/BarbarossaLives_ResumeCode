ba1b16eee2323ee0ad3f2a6b0ff2b414ba1c7a2d
BarbarossaLives <montebruce@proton.me>
2023-12-28 14:32:01 -0600

    MBD

--- NumberGuessing/NumberGuessingTTKBootstrap.py
+++ NumberGuessing/NumberGuessingTTKBootstrap.py
@@ -6,15 +6,22 @@ from ttkbootstrap.constants import *
 root = ttk.Window()
 style = ttk.Style("darkly")
 
-frame_left = ttk.Labelframe(root,bootstyle="primary")
-frame_right = ttk.Labelframe(root,bootstyle="primary")
+#frame_left = ttk.Labelframe(root,bootstyle="PRIMARY")
+#frame_right = ttk.Labelframe(root,bootstyle="SUCCESS")
 
-label1 = ttk.Label(frame_left,text="Pick you number ")
+label1 = ttk.Label(root,text="Pick you number between 1 and 100 ")
+label2 = ttk.Label(root,text="The computer has picked a number")
+label3 = ttk.Label(root,text="Pick your number.")
 
-frame_left.pack()
-frame_right.pack()
-label1.pack()
 
+root.columnconfigure(0,weight=1)
+root.columnconfigure(1,weight=1)
+root.rowconfigure(0,weight=1)
+
+#frame_left.grid(row=1, column=1)
+#frame_right.grid(row=1, column=2)
+label1.grid(column=0,row=1,padx=10)
+label2.grid(column=1,row=1,padx=10)
 
 
 root.mainloop()
